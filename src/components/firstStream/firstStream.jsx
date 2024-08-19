37c56310b0899d068c4de0b184e44697ec17436a
Gabriel Preller <gpreller@geovictoria.com>
2024-08-19 16:33:20 -0400

    Se añade primer stream

--- src/components/firstStream/firstStream.jsx
+++ src/components/firstStream/firstStream.jsx
@@ -1,19 +1,24 @@
 import { Container, Image } from "react-bootstrap"
-import beleny19 from '../../assets/images/beleny19.webp'
+import firstStream from '../../assets/videos/primer-stream-1.mp4'
+import './firstStream.css'
 
 const FirstStream = () => {
     return (
-        <Container className="d-flex flex-column justify-content-center align-items-center min-vh-100" >
-            <Container className="d-flex flex-column justify-content-center align-items-center">
-                <Image src={beleny19}  />
-                <h2 className="text-white text-center">
-                    19 de Agosto
-                </h2>
-                <h2 className="text-white text-center">
-                    Aniversario de Partner
-                </h2>
+        <Container className="d-flex flex-column justify-content-center align-items-center min-vh-100 photo-container">
+            <Container className="d-flex flex-column justify-content-center align-items-center min-vh-100 demo-content" >
+                <Container className="pb-3">
+                    <h2 className="text-white text-center">
+                        Primer stream 👀
+                    </h2>
+                </Container>
+                <Container className="d-flex flex-column justify-content-center align-items-center">
+                    <video controls>
+                        <source src={firstStream} />
+                    </video>
+                </Container>
             </Container>
         </Container>
+        
     )
 }
 
