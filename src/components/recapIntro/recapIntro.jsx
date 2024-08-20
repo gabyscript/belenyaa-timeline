677fd5dfd58287b3d955fb1dacc0bd0c7daca8d7
Gabriel Preller <gpreller@geovictoria.com>
2024-08-19 21:36:24 -0400

    Cambio historia en twitch

--- src/components/recapIntro/recapIntro.jsx
+++ src/components/recapIntro/recapIntro.jsx
@@ -7,12 +7,9 @@ const RecapIntro = () => {
         <Container className="d-flex flex-column justify-content-center align-items-center min-vh-100 background" >
             <Container className="d-flex flex-column justify-content-center align-items-center home-container w-75 py-5">
                 <Image src={beleny19}  />
-                <h1 className="text-white text-center neon">
-                    19 de Agosto
+                <h1 className="text-white text-center neon text-uppercase">
+                    Tu historia en Twitch
                 </h1>
-                <h2 className="text-white text-center neon">
-                    Aniversario de Partner
-                </h2>
             </Container>
         </Container>
     )
